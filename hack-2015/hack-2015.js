af57889603f633ab5fd770a41af394f3aab3d40a
Rose Wang <rzwang@mit.edu>
2015-09-19 22:04:29 -0400

    works so far

--- hack-2015/hack-2015.js
+++ hack-2015/hack-2015.js
@@ -4,19 +4,22 @@ if (Meteor.isClient) {
   Template.messages.helpers({
     callPolice: function () {
       var cursor = DangerScore.find().fetch();
-      var score = cursor[0]["current"];
+      var score = cursor[0]["score"];
+      console.log(cursor[0]);
       return score >= .75;
     },
 
     checkSecurity: function () {
       var cursor = DangerScore.find().fetch();
       var score = cursor[0]["current"];
+      console.log(cursor[0]);
       return (score >= .5 && score < .75);
     },
 
     allGood: function () {
       var cursor = DangerScore.find().fetch();
       var score = cursor[0]["current"];
+      console.log(cursor[0]);
       return score < .5;
     }
 
